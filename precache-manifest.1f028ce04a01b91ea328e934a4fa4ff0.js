1d29f211cdf980443bed18646023457f08a7b323
sifat <sifat@sifat.local>
2020-05-26 22:21:46 +0600

    Updates

---
+++ precache-manifest.1f028ce04a01b91ea328e934a4fa4ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f37dbd1dbad4ccaf49eedae342706ba",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "fc1389ddba6ff1386951",
+    "url": "/ecommerce/static/css/main.c31425e2.chunk.css"
+  },
+  {
+    "revision": "44263e594dd763556074",
+    "url": "/ecommerce/static/js/2.e148234e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce/static/js/2.e148234e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1389ddba6ff1386951",
+    "url": "/ecommerce/static/js/main.f55cbbd4.chunk.js"
+  },
+  {
+    "revision": "d3b889ef94a52edad700",
+    "url": "/ecommerce/static/js/runtime-main.f14a8a50.js"
+  }
+]);
