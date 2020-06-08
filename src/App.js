d69218fe3fb38c452c117c1b7c32fb2f32e8f784
sifat <sifat@sifat.local>
2020-06-08 23:09:07 +0600

    added menu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop-page/shop-page.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+import "./header.styles.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="menu-items">
+        <Link className="menu-item" to="/shop">
+          Shop
+        </Link>
+        <Link className="menu-item" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
